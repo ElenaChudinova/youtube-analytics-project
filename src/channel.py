e5137f8173fe5caf59c84f8c444e65d55d0ef418
ElenaChudinova <maknali@yandex.ru>
2023-10-10 20:15:01 +0300

    new correct cod_4

--- src/channel.py
+++ src/channel.py
@@ -9,13 +9,37 @@ path = "./moscowpython.json"
 class Channel:
     """Класс для ютуб-канала"""
 
-
     def __init__(self, channel_id):
         self.channel = self.get_service().channels().list(id=channel_id, part='snippet,statistics').execute()
         self.__channel_id = channel_id
         self.title = self.channel["items"][0]["snippet"]["title"]
         self.video_count = self.channel["items"][0]["statistics"]["videoCount"]
-        self.url = 'https://www.youtube.com/channel/'
+        self.url = 'https://www.youtube.com/channel/' + self.channel["items"][0]["id"]
+        self.subscriber_count = self.channel["items"][0]["statistics"]["subscriberCount"]
+
+    def __str__(self):
+        return f"{self.title} ({self.url})"
+
+    def __add__(self, other):
+        return int(self.subscriber_count) + int(other.subscriber_count)
+
+    def __sub__(self, other):
+        return int(self.subscriber_count) - int(other.subscriber_count)
+
+    def __lt__(self, other):
+        return int(self.subscriber_count) > int(other.subscriber_count)
+
+    def __le__(self, other):
+        return int(self.subscriber_count) >= int(other.subscriber_count)
+
+    def __gt__(self, other):
+        return int(self.subscriber_count) < int(other.subscriber_count)
+
+    def __ge__(self, other):
+        return int(self.subscriber_count) <= int(other.subscriber_count)
+
+    def __eq__(self, other):
+        return int(self.subscriber_count) == int(other.subscriber_count)
 
     @property
     def channel_id(self):
@@ -28,17 +52,14 @@ class Channel:
         else:
             self.__channel_id = channel_id
 
-
     @classmethod
     def get_service(cls):
         return build('youtube', 'v3', developerKey=os.environ.get('API-KEY-YT'))
-
 
     def get_info(self):
         youtube = self.get_service()
         response = youtube.channels().list(id=self.__channel_id, part='snippet,statistics').execute()
         return response
-
 
     def print_info(self):
         info_channel = {}
@@ -51,8 +72,6 @@ class Channel:
         info_channel['количество видео'] = response["items"][0]["statistics"]["videoCount"]
         info_channel['количество просмотров'] = response["items"][0]["statistics"]["viewCount"]
         return info_channel
-
-
 
     def to_json(self, path):
         response = self.print_info()
